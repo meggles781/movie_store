33ebcb8e1fa53e36b09de1b8df50fbc28121e282
“Meg <meg5ed@virginia.edu>
2021-12-04 09:34:14 -0700

    finished stores

--- app/javascript/components/Stores.js
+++ app/javascript/components/Stores.js
@@ -2,9 +2,19 @@ import React from "react";
 
 
 const Stores = (props) => {
+
+  const { stores } = props;
+
+  const renderStores = () =>  {
+    return stores.map((store) => {
+      return  <p>(store.name)</p>;
+      
+    })
+  }
   return (
     <div>
       <h1>Stores</h1>
+      {renderStores()}
     </div>
   )
 }
